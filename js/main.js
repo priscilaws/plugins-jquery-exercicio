cd62b6463baa47c9e2bb359e0c165a996845517c
Priscila <pri.wagner.silva@gmail.com>
2024-12-06 18:42:22 +0000

    Resolucao do exercicio

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $('#carousel-images').slick({
+        autoplay: true,
+        arrows: false
+    })
+})
+
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+})
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder: '012345-678'
+})
+
+$('form').validate({
+    rules: {
+        nome: {
+            requerido: true
+        },
+        email: {
+            requerido: true,
+            email: true
+        },
+        telefone: {
+            requerido: true
+        },
+        endereco: {
+            requerido: true
+        },
+        cep: {
+            requerido: true
+        },
+        cpf: {
+            requerido: true
+        },
+    },
+    submitHandler: function (form) {
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+        form.reset();
+    },
+    invalidHandler: function (form, validator) {
+        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    }
+})
